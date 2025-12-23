114ae685420024cd52e4fb9f282b3f14586d7aaf
sharad1666 <sharad.dac@gmail.com>
2025-12-23 15:45:02 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import { SocketProvider } from "./context/SocketProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <SocketProvider>
       <App />
     </SocketProvider>
-  </BrowserRouter>
+  </React.StrictMode>
 );
