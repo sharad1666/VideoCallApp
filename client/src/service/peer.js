511937d46e4b2cb7c561b59e83f9c4b5567f44e2
sharad1666 <sharad.dac@gmail.com>
2025-12-23 15:02:57 +0530

    Update peer.js

--- client/src/service/peer.js
+++ client/src/service/peer.js
@@ -1,5 +1,10 @@
 class PeerService {
   constructor() {
+    this.peer = null;
+    this.createPeer();
+  }
+
+  createPeer() {
     this.peer = new RTCPeerConnection({
       iceServers: [
         { urls: "stun:stun.l.google.com:19302" },
@@ -9,20 +14,51 @@ class PeerService {
   }
 
   async getOffer() {
+    if (!this.peer) this.createPeer();
+
     const offer = await this.peer.createOffer();
     await this.peer.setLocalDescription(offer);
     return offer;
   }
 
   async getAnswer(offer) {
-    await this.peer.setRemoteDescription(new RTCSessionDescription(offer));
+    if (!this.peer) this.createPeer();
+
+    await this.peer.setRemoteDescription(
+      new RTCSessionDescription(offer)
+    );
+
     const answer = await this.peer.createAnswer();
     await this.peer.setLocalDescription(answer);
     return answer;
   }
 
   async setRemoteAnswer(answer) {
-    await this.peer.setRemoteDescription(new RTCSessionDescription(answer));
+    if (!this.peer) this.createPeer();
+
+    await this.peer.setRemoteDescription(
+      new RTCSessionDescription(answer)
+    );
+  }
+
+  // 🔄 Replace video track (used for screen sharing)
+  replaceVideoTrack(newTrack) {
+    const sender = this.peer
+      ?.getSenders()
+      ?.find((s) => s.track && s.track.kind === "video");
+
+    if (sender) {
+      sender.replaceTrack(newTrack);
+    }
+  }
+
+  // ❌ End call & reset peer
+  closePeer() {
+    if (this.peer) {
+      this.peer.getSenders().forEach((sender) => sender.track?.stop());
+      this.peer.close();
+      this.peer = null;
+    }
   }
 }
 
